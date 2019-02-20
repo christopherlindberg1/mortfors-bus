95512d242386694e9c0ac5ea1d123dec228f8e9b
Christopher Lindberg <christopher.lindberg1@gmail.com>
2019-02-21 00:04:31 +0100

    Changed datatype for price. Inserted new trips

--- db-queries.sql
+++ db-queries.sql
@@ -2,11 +2,11 @@
 -- applikationen ska ha något innehåll
 
 
-INSERT INTO driver
-VALUES 	('671215-6525', 'Jonas', 'Davidsson', 'Sverige', 'Malmö', '223 86', 'Gröna stigen 13', '040-336512'),
+INSERT INTO driver VALUES
+		('671215-6525', 'Jonas', 'Davidsson', 'Sverige', 'Malmö', '603 86', 'Gröna stigen 13', '040-336512'),
 		('690409-7959', 'David', 'Eliasson', 'Sverige', 'Lund', '253 1', 'Blåa stigen 3', '048-795921'),
 		('760205-1279', 'Christopher', 'Lindberg', 'Sverige', 'Malmö', '243 96', 'Gula gatan 7', '042-369512'),
-		('711118-7692', 'Robert', 'Bergqvist', 'Sverige', 'Lund', '253 76', 'Grusvägen 14', '040-851222'),
+		('711118-7692', 'Robert', 'Bergqvist', 'Sverige', 'Lund', '253 76', 'Grusvägen 14', '040-851602'),
 		('900915-6390', 'Jesper', 'Jönsson', 'Sverige', 'Helsingborg', '253 86', 'Hbgvägen 13', '065-683112'),
 		('660615-6526', 'Gunnel', 'Samuelsson', 'Sverige', 'Göteborg', '353 1', 'Isbanan 67', '076-239043'),
 		('590215-1395', 'Bosse', 'Kvist', 'Sverige', 'Stockholm', '143 96', 'Blåhakevägen 4', '044-590939'),
@@ -16,111 +16,40 @@ VALUES 	('671215-6525', 'Jonas', 'Davidsson', 'Sverige', 'Malmö', '223 86', 'Gr
 		('910404-8943', 'Henrik', 'Gustavsson', 'Sverige', 'Kiruna', '953 76', 'Snöstigen 40', '060-523942');
 
 
-INSERT INTO city (city_name, country, post_nr, street)
-VALUES	('Malmö', 'Sverige', '212 54', 'Manetgatan 6'),
+INSERT INTO city (city_name, country, post_nr, street) VALUES
+		('Mörtfors', 'Sverige', '345 23', 'Mörtegatan 12')
+		('Malmö', 'Sverige', '212 54', 'Manetgatan 6'),
 		('Helsingborg', 'Sverige', '259 45', 'Snäckgatan 50'),
 		('Ystad', 'Sverige', '283 34', 'Astagatan 1'),
 		('Göteborg', 'Sverige', '324 54', 'Ripgatan 9'),
-		('Stockholm', 'Sverige', '111 11', 'Rungatan 22'),
+		('Stockholm', 'Sverige', '111 11', 'Rungatan 60'),
 		('Uppsala', 'Sverige', '580 95', 'Elinsgatan 75'),
 		('Kiruna', 'Sverige', '879 95', 'Dorisgatan 14'),
 		('Köpenhamn', 'Danmark', '124 542', 'Evagatan 8'),
 		('Berlin', 'Tyskland', '124 864', 'Lövsångaregatan 67'),
-		('Paris', 'Frankrike', '584 922', 'Fosievägen 54'),
+		('Paris', 'Frankrike', '584 960', 'Fosievägen 54'),
 		('Oslo', 'Norge', '549 49', 'Stjärngatan 12');
 
 
-INSERT INTO trip (startdest, enddest, starttime, arrival, price, empty_seats, driver)
-VALUES	('Malmö', 'Helsingborg', '2018-08-20 08:00:00', '2018-08-20 09:00:00', '99', 22, '671215-6525'),
-		('Malmö', 'Ystad', '2018-08-20 08:00:00', '2018-08-20 09:00:00', '99', 22, '690409-7959'),
-		('Stockholm', 'Uppsala', '2018-08-21 10:00:00', '2018-08-20 14:00:00', '199', 22, '711118-7692'),
-		('Göteborg', 'Ystad', '2018-08-21 09:40:00', '2018-08-20 11:25:00', '99', 22, '900915-6390'),
-		('Malmö', 'Paris', '2018-08-20 07:40:00', '2018-08-20 21:30:00', '399', 22, '660615-6526'),
-		('Malmö', 'Oslo', '2018-08-20 08:40:00', '2018-08-21 12:00:00', '599', 22, '590215-1395'),
-		('Uppsala', 'Köpenhamn', '2018-08-20 09:00:00', '2018-08-20 23:00:00', '399', 22, '880808-5494'),
-		('Köpenhamn', 'Uppsala', '2018-08-21 08:00:00', '2018-08-20 22:00:00', '399', 22, '851609-5490'),
-		('Göteborg', 'Stockholm', '2018-08-22 08:00:00', '2018-08-22 17:00:00', '299', 22, '761123-6992'),
-		('Göteborg', 'Uppsala', '2018-08-22 08:00:00', '2018-08-20 17:00:00', '199', 22, '671215-6525'),
-		('Stockholm', 'Paris', '2018-08-22 10:00:00', '2018-08-20 14:00:00', '599', 22, '910404-8943'),
+INSERT INTO trip (startdest, enddest, starttime, arrival, price, empty_seats, driver) VALUES
+		('Mörtfors', 'Stockholm', '2019-04-03 10:50:00', '2019-04-03 14:30:00', 99, 60, '671215-6525'),
+		('Stockholm', 'Mörtfors', '2018-04-07 10:00:00', '2019-04-07 14:20:00', 99, 60, '690409-7959'),
+		('Mörtfors', 'Malmö', '2019-04-03 09:30:00', '2019-04-03 14:00:00', 99, 60, '711118-7692'),
+		('Malmö', 'Mörtfors', '2019-04-07 09:40:00', '2019-04-07 14:10:00', 99, 60, '900915-6390'),
+		('Mörtfors', 'Göteborg', '2019-04-04 09:50:00', '2019-04-04 13:40:00', 99, 60, '660615-6526'),
+		('Göteborg', 'Mörtfors', '2019-04-08 10:40:00', '2019-04-08 14:30:00', 99, 60, '590215-1395'),
+		('Malmö', 'Göteborg', '2019-04-03 12:00:00', '2019-04-03 16:40:00', 99, 60, '880808-5494'),
+		('Göteborg', 'Malmö', '2019-04-07 12:00:00', '2019-04-07 16:40:00', 99, 60, '851609-5490'),
+		('Malmö', 'Stockholm', '2019-04-05 09:00:00', '2019-04-05 18:20:00', 99, 60, '761123-6992'),
+		('Stockholm', 'Malmö', '2019-04-09 09:00:00', '2019-04-09 18:20:00', 99, 60, '671215-6525'),
+		('Stockholm', 'Göteborg', '2019-04-04 10:00:00', '2019-04-04 14:30:00', 99, 60, '910404-8943'),
+		('Göteborg', 'Stockholm', '2019-04-08 10:00:00', '2019-04-07 14:30:00', 99, 60, '910404-8943'),
 
-		('Malmö', 'Helsingborg', '2018-08-23 08:00:00', '2018-08-23 09:00:00', '99', 22, '671215-6525'),
-		('Malmö', 'Ystad', '2018-08-23 08:00:00', '2018-08-23 09:00:00', '99', 22, '690409-7959'),
-		('Stockholm', 'Uppsala', '2018-08-23 10:00:00', '2018-08-23 14:00:00', '199', 22, '711118-7692'),
-		('Göteborg', 'Ystad', '2018-08-23 09:40:00', '2018-08-23 11:25:00', '99', 22, '900915-6390'),
-		('Malmö', 'Paris', '2018-08-23 07:40:00', '2018-08-23 21:30:00', '399', 22, '660615-6526'),
-		('Malmö', 'Oslo', '2018-08-23 08:40:00', '2018-08-23 12:00:00', '599', 22, '590215-1395'),
-		('Uppsala', 'Köpenhamn', '2018-08-23 09:00:00', '2018-08-23 23:00:00', '399', 22, '880808-5494'),
-		('Köpenhamn', 'Uppsala', '2018-08-23 08:00:00', '2018-08-23 22:00:00', '399', 22, '851609-5490'),
-		('Göteborg', 'Stockholm', '2018-08-23 08:00:00', '2018-08-23 17:00:00', '299', 22, '761123-6992'),
-		('Göteborg', 'Uppsala', '2018-08-23 08:00:00', '2018-08-23 17:00:00', '199', 22, '671215-6525'),
-		('Stockholm', 'Paris', '2018-08-23 10:00:00', '2018-08-23 14:00:00', '599', 22, '910404-8943'),
-
-		('Malmö', 'Helsingborg', '2018-08-26 08:00:00', '2018-08-26 09:00:00', '99', 22, '671215-6525'),
-		('Malmö', 'Ystad', '2018-08-26 08:00:00', '2018-08-26 09:00:00', '99', 22, '690409-7959'),
-		('Stockholm', 'Uppsala', '2018-08-26 10:00:00', '2018-08-26 14:00:00', '199', 22, '711118-7692'),
-		('Göteborg', 'Ystad', '2018-08-26 09:40:00', '2018-08-26 11:25:00', '99', 22, '900915-6390'),
-		('Malmö', 'Paris', '2018-08-26 07:40:00', '2018-08-26 21:30:00', '399', 22, '660615-6526'),
-		('Malmö', 'Oslo', '2018-08-26 08:40:00', '2018-08-26 12:00:00', '599', 22, '590215-1395'),
-		('Uppsala', 'Köpenhamn', '2018-08-26 09:00:00', '2018-08-26 23:00:00', '399', 22, '880808-5494'),
-		('Köpenhamn', 'Uppsala', '2018-08-26 08:00:00', '2018-08-26 22:00:00', '399', 22, '851609-5490'),
-		('Göteborg', 'Stockholm', '2018-08-26 08:00:00', '2018-08-26 17:00:00', '299', 22, '761123-6992'),
-		('Göteborg', 'Uppsala', '2018-08-26 08:00:00', '2018-08-26 17:00:00', '199', 22, '671215-6525'),
-		('Stockholm', 'Paris', '2018-08-26 10:00:00', '2018-08-26 14:00:00', '599', 22, '910404-8943');
-
-
--- SQL-frågorna nedan körs via servern när användaren interagerar
--- med applikationen
-
-
--- Registrerar en ny användare
-INSERT INTO customer VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s),
-(email, firstname, lastname, country, city, post_nr, street, tel_nr, password)
-
-
--- Hämtar all info om kunden med den email som angavs vid inloggning.
--- Används för att matcha lösenordet som skrivs in med det registrerade
--- lösenordet i databasen
-"SELECT * FROM customer WHERE email = %s",
-[email];
-
-
--- Hämtar info om alla tillgängliga turer
-"SELECT * FROM trip ORDER BY startdest, enddest, starttime"
-
-
--- Updaterar antalet lediga platser för en tur när en bokning görs
-"UPDATE trip SET empty_seats = %s WHERE trip_id = %s",
-[seats_left, trip_id]
-
-
--- Registrerar en bokning tillhörande den inloggade personen
-"INSERT INTO booking VALUES (%s, %s, %s)", [session["email"],
-trip_id, nr_of_seats]
-
-
--- Hämtar turer som den inloggade personen har bokat
-"SELECT t.trip_id, t.startdest, t.enddest, t.starttime, t.arrival, b.nr_of_seats
-FROM trip as t
-INNER JOIN booking as b
-ON b.email = %s AND t.trip_id=b.trip_id
-ORDER BY starttime",
-[session["email"]]
-
-
--- Hämtar information om en tur som en kund har bokat
-"""SELECT b.email, b.trip_id, b.nr_of_seats, t.trip_id, t.startdest,
-t.enddest, t.starttime, t.arrival, t.price, t.empty_seats
-FROM booking as b
-JOIN trip as t
-ON b.trip_id = %s b.trip_id = t.trip_id AND b.email = %s""",
-[trip_id, session["email"]]
-
-
--- Updaterar antalet biljetter en kund har till en viss tur
-"""UPDATE booking SET nr_of_seats = %s WHERE email = %s AND trip_id = %s""",
-[nr_of_seats, session["email"], trip_id]
-
-
--- Tar bort en kunds bokning
-"DELETE FROM booking WHERE trip_id = %s AND email = %s",
-	[trip_id, session["email"]]
+		('Mörtfors', 'Köpenhamn', '2019-04-03 10:00:00', '2019-04-07 14:00:00', 199, 60, '910404-8943'),
+		('Köpenhamn', 'Mörtfors', '2019-04-07 10:00:00', '2019-04-07 14:00:00', 199, 60, '910404-8943'),
+		('Stockholm', 'Köpenhamn', '2019-04-03 09:00:00', '2019-04-03 17:50:00', 199, 60, '910404-8943'),
+		('Köpenhamn', 'Stockholm', '2019-04-07 09:00:00', '2019-04-07 17:50:00', 199, 60, '910404-8943'),
+		('Göteborg', 'Köpenhamn', '2019-04-03 10:00:00', '2019-04-03 17:10:00', 199, 60, '910404-8943'),
+		('Köpenhamn', 'Göteborg', '2019-04-07 10:00:00', '2019-04-07 17:10:00', 199, 60, '910404-8943'),
+		('Malmö', 'Köpenhamn', '2019-04-03 12:00:00', '2019-04-07 12:30:00', 99, 60, '910404-8943'),
+		('Köpenhamn', 'Malmö', '2019-04-07 12:00:00', '2019-04-07 12:30:00', 99, 60, '910404-8943');
--- db-schema.sql
+++ db-schema.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE morfors;
 
-CREATE TABLE customer  					-- information om kunderna
+
+-- information about customers
+CREATE TABLE customer
 	(email VARCHAR(50) NOT NULL,
 	firstname VARCHAR(30) NOT NULL,
 	lastname VARCHAR(30) NOT NULL,
@@ -13,7 +15,8 @@ CREATE TABLE customer  					-- information om kunderna
 	PRIMARY KEY (email));
 
 
-CREATE TABLE driver  					-- information om chaufförerna
+-- information about bus drivers
+CREATE TABLE driver
 	(pers_nr CHAR(11) NOT NULL,
 	firstname VARCHAR(30) NOT NULL,
 	lastname VARCHAR(30) NOT NULL,
@@ -25,32 +28,35 @@ CREATE TABLE driver  					-- information om chaufförerna
 	PRIMARY KEY (pers_nr));
 
 
-CREATE TABLE city 						-- information om städerna/hållplatserna bolaget kör till
+-- Information about the cities the company has trips to
+CREATE TABLE city
 	(city_name VARCHAR(30) NOT NULL,
-	country VARCHAR(30) NOT NULL,
-	post_nr VARCHAR(10) NOT NULL,
-	street VARCHAR(30) NOT NULL,
-	PRIMARY KEY (city_name));			-- hållplatsens adress
+	country VARCHAR(30) NOT NULL,		-- country the city belongs to
+	post_nr VARCHAR(10) NOT NULL,		-- ZIP code for bus stop
+	street VARCHAR(30) NOT NULL,		-- street address for the bus stop
+	PRIMARY KEY (city_name));
 
 
-CREATE TABLE trip	  					-- företagets alla planerade turer
+-- All trips that
+CREATE TABLE trip
 	(trip_id INT AUTO_INCREMENT,
-	startdest VARCHAR(30) NOT NULL,		-- startdestinationens stad
-	enddest VARCHAR(30) NOT NULL,		-- ankomstdestinationens stad
-	starttime DATETIME NOT NULL,  		-- tid och datum för avgång
-	arrival DATETIME NOT NULL,  		-- tid och datum för ankomst
-	price VARCHAR(10) NOT NULL,  		-- pris per person
-	empty_seats INT NOT NULL,  			-- antal lediga platser
-	driver CHAR(11),					-- chaufför för resan
+	startdest VARCHAR(30) NOT NULL,	-- city of the start destination
+	enddest VARCHAR(30) NOT NULL,		-- city of the end destination
+	starttime DATETIME NOT NULL,  	-- date and time for when the trip begins
+	arrival DATETIME NOT NULL,  		-- date and time for when the bus arrives
+	price INT NOT NULL,  						-- price per person
+	empty_seats INT NOT NULL,  			-- nr of available seats
+	driver CHAR(11),								-- driver
 	PRIMARY KEY (trip_id),
 	FOREIGN KEY (startdest) references city(city_name),
 	FOREIGN KEY (enddest) references city(city_name));
 
 
-CREATE TABLE booking					-- kundernas bokade turer
-	(email VARCHAR(50) NOT NULL,		-- kundens email
-	trip_id INT NOT NULL,				-- id för turen som kunden bokat
-	nr_of_seats INT NOT NULL,			-- antalet platser som kunden bokar
+-- bookings
+CREATE TABLE booking
+	(email VARCHAR(50) NOT NULL,	-- customer's email
+	trip_id INT NOT NULL,					-- id for trip
+	nr_of_seats INT NOT NULL,			-- nr of tickets the customer has bought
 	PRIMARY KEY (email, trip_id),
 	FOREIGN KEY (email) references customer (email),
 	FOREIGN KEY (trip_id) references trip (trip_id));
