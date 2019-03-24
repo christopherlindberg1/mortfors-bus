5f7a8735468b82bb0034eece126441db20a65563
Christopher Lindberg <christopher.lindberg1@gmail.com>
2019-03-24 13:04:58 +0100

    File with all destinations, will be used for drop down menu in a form

---
+++ destinations.py
@@ -0,0 +1,16 @@
+data = [
+    'Mortfors',
+    'Malmo',
+    'Helsingborg',
+    'Ystad',
+    'Gothenburg',
+    'Stockholm',
+    'Uppsala',
+    'Kiruna',
+    'Copenhagen',
+    'Berlin',
+    'Paris',
+    'Oslo'
+]
+
+destinations = [(n, n) for n in data]
