f4274bceefebca229d85fc8bb14461a06cec1615
Christopher Lindberg <christopher.lindberg1@gmail.com>
2019-03-24 13:03:16 +0100

    Added form classes for creating trips and adding destination. Not complete yet

--- forms.py
+++ forms.py
@@ -1,22 +1,42 @@
-from wtforms import Form, StringField, TextAreaField, PasswordField, SelectField, BooleanField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField
+from wtforms import SelectField, BooleanField, IntegerField
+from wtforms.validators import Email, DataRequired, EqualTo, Optional
 import countries
+import destinations
 
 
 class RegistrationForm(Form):
-    email = StringField("Email", [validators.Email()])
-    firstname = StringField("First name", [validators.DataRequired()])
-    lastname = StringField("Last name", [validators.DataRequired()])
+    email = StringField("Email", validators=[Email()])
+    firstname = StringField("First name", validators=[DataRequired()])
+    lastname = StringField("Last name", validators=[DataRequired()])
     country = SelectField(u"Country", choices=countries.countries)
-    city = StringField("City", [validators.DataRequired()])
-    post_nr = StringField("ZIP code", [validators.DataRequired()])
-    street = StringField("Street", [validators.DataRequired()])
+    city = StringField("City", validators=[DataRequired()])
+    post_nr = StringField("ZIP code", validators=[DataRequired()])
+    street = StringField("Street", validators=[DataRequired()])
     tel_nr = StringField("Phone")
-    password = PasswordField("Password", [validators.DataRequired(),
-            validators.EqualTo("confirm", message="Fel lösenord")])
+    password = PasswordField("Password", validators=[DataRequired(),
+            EqualTo("confirm", message="Fel lösenord")])
     confirm = PasswordField("Confirm password")
 
 
 class LoginForm(Form):
-    email = StringField("Email", [validators.Email()])
-    password = PasswordField("Password", [validators.DataRequired()])
+    email = StringField("Email", validators=[Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember me")
+
+
+class AddDestinationForm(Form):
+    city_name = StringField("City", validators=[DataRequired()])
+    country = SelectField(u"Country", choices=countries.countries)
+    post_nr = StringField("ZIP code", validators=[DataRequired()])
+    street = StringField("Street", validators=[DataRequired()])
+
+
+class CreateTripForm(Form):
+    startdest = SelectField(u"City", choices=destinations.destinations)
+    enddest = SelectField(u"City", choices=destinations.destinations)
+    # departure = # Timestamp
+    # arrival = # Timestamp
+    price = IntegerField("price", 99)
+    driver = SelectField(u"City", choices=drivers.drivers,
+            validators=[Optional()])
